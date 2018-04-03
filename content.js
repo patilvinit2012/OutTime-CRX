6f8bd35d5d4f168dc6e0161b932aade0a8fbd29b
patilvinit2012 <14797444+patilvinit2012@users.noreply.github.com>
2018-04-03 11:08:55 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,3 +1,37 @@
-/*var firstHref = $("a[href^='http']").eq(0).attr("href");
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+script.type ="text/javascript";
+document.body.appendChild(script);
 
-console.log(firstHref);*/
+function getDayOfWeek(date) {
+  var dayOfWeek = new Date(date).getDay();    
+  return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+}
+
+var todaysBox;
+var weekIndex;
+
+var inTimeId = 'ctl00_cphPage_lblInTime';
+$('.bold-black-header').each(function(){
+	if($(this).text() == new Date().getDate())
+	{
+		todaysBox = $(this).closest('.data');
+		weekIndex =  todaysBox.attr('id').slice(-1);
+		inTimeId = inTimeId + getDayOfWeek(new Date()) + weekIndex;
+	}
+})
+var inTime = $('#'+inTimeId).text();
+copyToClipboard(inTime);
+
+localStorage.setItem('mastekInTime',inTime);
+
+
+
+function copyToClipboard(text){
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
--- popup.js
+++ popup.js
@@ -1,121 +1,7 @@
-/*function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-//Change the background color of the current page.@param {string} color The new background color.
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
+  //chrome.browserAction.setBadgeText({text: "8h"});
+  chrome.tabs.executeScript(null, {file: "content.js"});
 
 
- // Gets the saved background color for url.
- 
- // @param {string} url URL whose background color is to be retrieved.
- // @param {function(string)} callback called with the saved background color for
- //     the given url on success, or a falsy value if no color is retrieved.
- 
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-
- // Sets the given background color for url.
- // @param {string} url URL for which background color is to be saved.
- // @param {string} color The background color to be saved.
- 
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-  
-  getCurrentTabUrl((url) => {
-    var dropdown = document.getElementById('dropdown');
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
-  });
-});
-*/
-
-
-
-
-//document.addEventListener('DOMContentLoaded', () => {
     const OUTTIME = 'OUTTIME';
     const INTIME = 'INTIME';
     const BUCKETTIME = 'BUCKETTIME';
@@ -123,8 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const DEFAULTBUCKHOUR =  8;
     const DEFAULTBUCKMINS = 45;
     const DEFAULTBUCKTIME = DEFAULTBUCKHOUR + ':' + DEFAULTBUCKMINS;
-
-
+    const LISTVIEW = 'LISTVIEW';
+    const ABSTRACTVIEW = 'ABSTRACTVIEW';
+    const DEFAULTVIEW = LISTVIEW;
+    const VIEWOPTION  = 'VIEWOPTION';
     init();
 
     $('#inputTime').bind('keyup',function(event){
@@ -132,6 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
         calculate();
       }
     })
+
+    $('#viewBtn').bind('click',changeView);
     
     var inTimeObjStr = localStorage.getItem(OUTTIME);
     var inTimeObj = JSON.parse(inTimeObjStr);
@@ -151,21 +41,41 @@ document.addEventListener('DOMContentLoaded', () => {
       var inputDate = getInpTime();
       var currDate = new Date();
       var timeElObj = msToTime(currDate - inputDate);
-      $('#timeElapsed').text(`${timeElObj.hrs} Hours , ${timeElObj.mins} Minutes`);
+      $('.timeElapsed').text(getFTimeText(timeElObj.hrs,timeElObj.mins));
 
       var estOutDate = new Date();
       estOutDate.setHours(inputDate.getHours() + (parseInt(localStorage[BUCKETTIME].split(':')[0]) || DEFAULTBUCKHOUR));
       var hours = estOutDate.getHours();
       estOutDate.setMinutes(inputDate.getMinutes() + (parseInt(localStorage[BUCKETTIME].split(':')[1]) || DEFAULTBUCKMINS));
       var minutes = estOutDate.getMinutes();
+      minutes = minutes > 10 ? minutes : "0" + minutes;
       var am_pm = hours >= 12 ? 'pm' : 'am';
-      $('#outputTime').text(`${estOutDate.getHours()}:${minutes} ${am_pm}`);
+      $('.outputTime').text(`${estOutDate.getHours()}:${minutes} ${am_pm}`);
 
 
       var reTimeObj = msToTime(estOutDate - currDate);
-      $('#timeRemaining').text(`${reTimeObj.hrs} Hours , ${reTimeObj.mins} Minutes`);
+      $('.timeRemaining').text(getFTimeText(reTimeObj.hrs,reTimeObj.mins));
+      chrome.browserAction.setBadgeText({text: reTimeObj.hrs + 'h+'});
+    }
 
-      
+    function getFTimeText(hrs,mins){
+      var text ='';
+      hrs = Number.parseInt(hrs);
+      mins = Number.parseInt(mins);
+      if(hrs){
+        text += `${hrs} ${singOrPlu('Hour',hrs)}, `;    
+      }
+      if(mins){
+        text += `${mins} ${singOrPlu('Minute',mins)}`;  
+      }
+      return text;
+    }
+
+    function singOrPlu(text,intVal){
+      if(intVal > 1){
+        return text+'s';
+      }
+      return text;
     }
 
     function getInpTime(){
@@ -192,15 +102,27 @@ document.addEventListener('DOMContentLoaded', () => {
       var inpTimeObj = $('#inputTime');
       if(!inpTimeObj.val()) inpTimeObj.val(DEFAULTTIME);
 
-      $('#bucketTime').text(localStorage.getItem(BUCKETTIME) || DEFAULTBUCKTIME);
-      localStorage[BUCKETTIME] = $('#bucketTime').text();
+      $('.bucketTime').text(localStorage.getItem(BUCKETTIME) || DEFAULTBUCKTIME);
+      localStorage[BUCKETTIME] = $('.bucketTime').text();
+
+      var view = localStorage.getItem(VIEWOPTION);
+      if(!view) {
+        localStorage.setItem(VIEWOPTION,DEFAULTVIEW);
+      }else{
+        $('#listView, #abstractView').hide();
+        if(view == LISTVIEW){
+          $('#listView').show();
+        }else if(view == ABSTRACTVIEW){
+          $('#abstractView').show();
+        }
+      }
     }
 
-    $('#bucketTime').on({
+    $('.bucketTime').on({
       click: function(){
-        var parentObj = $('#bucketTimeCtnr');
-        var bucketTimeVal = $('#bucketTime').text();
-        $('#bucketTime').hide();
+        var parentObj = $('.bucketTimeCtnr');
+        var bucketTimeVal = $('.bucketTime').text();
+        $('.bucketTime').hide();
         parentObj.append(`<input type="text" id="bucketTimeInp" class="" style="width:10%" placeholder="Eg: 8:45">`);
         $('#bucketTimeInp').val(bucketTimeVal);
         $('#bucketTimeInp').on('keyup',event => onBuckeTimeInp(event));
@@ -212,10 +134,10 @@ document.addEventListener('DOMContentLoaded', () => {
           var timeStr = $('#bucketTimeInp').val();
         localStorage.setItem(BUCKETTIME,timeStr);
         $('#bucketTimeInp').remove();
-        $('#bucketTime').show().text(timeStr);
+        $('.bucketTime').show().text(timeStr);
         calculate();
           }else if(event.keyCode == 27){
-            $('#bucketTime').show();
+            $('.bucketTime').show();
             $('#bucketTimeInp').remove();
           }
     }
@@ -227,5 +149,18 @@ document.addEventListener('DOMContentLoaded', () => {
           var editTemplate = `<input type="text" id="dynEditNote" class="form-control" value="${text}" autofocus="autofocus">`;
           parentObj.append(editTemplate);
           $('#dynEditNote').on('keyup',event => saveDynNote(event,$('#dynEditNote')));
-        }
+    }
+
+
+    function changeView(){  
+      var viewOption = localStorage.getItem(VIEWOPTION);
+      $('#listView, #abstractView').hide();
+      if(viewOption == LISTVIEW){
+        localStorage.setItem(VIEWOPTION,ABSTRACTVIEW);
+        $('#abstractView').show();
+      }else if (viewOption == ABSTRACTVIEW) {
+        localStorage.setItem(VIEWOPTION,LISTVIEW);
+        $('#listView').show();
+      }
+    }
 //})
